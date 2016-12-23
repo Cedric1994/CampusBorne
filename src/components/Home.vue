c650abe8897ddd26859d7c67c6089df213821b5f
Thomas Clop <clop@polytech.unice.fr>
2016-12-23 17:58:38 +0100

    CHANGED Home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,30 @@
 <template>
-  <!--div
-    div(class="header")-->
-    <!-- <h2 id="date" v-el:date>test</h2> -->
-      <!--h2(id="date") Mardi 8 Novembre
-      h2(id="time") 10h30
-    hr-->
+  <div>
+    <div class="header">
+      <h2 id="date">Mardi 8 Novembre</h2>
+      <h2 id="time">10h30</h2>
+    </div>
+    <hr/>
+    <router-link to="FindDestination" class="button">Trouver ma destination</router-link>
+    <router-link to="FreeClassRoom" class="button">Trouver un salle libre</router-link>
+    <router-link to="Edt" class="button">Consulter mon emploi du temps</router-link>
+  </div>
 </template>
 
 <script>
+  import Backbutton from './common/BackButton'
   export default {
     name: 'Home',
-    data() {
-      return{
-
-      }
+    components: {
+      Backbutton
     }
-    // created() {
-    //   var time = this.$els.date;
-    //   time.textContent = "9h48";
-    // }
   }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .header{
-  background-color: yellow;
+  padding-left: 5%;
+  padding-right: 5%;
   height: 4em;
   line-height: 4em;
   white-space: nowrap;
@@ -34,12 +33,10 @@
 #date{
   float: left;
   margin: 0;
-  background: red;
 }
 
 #time{
   float: right;
   margin: 0;
-  background: blue;
 }
 </style>
