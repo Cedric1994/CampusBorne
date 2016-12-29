554d6f83f6c321833e3f3cc77d0a29038202bf4f
Zhengqin YAN <yanzhengqin1993@gmail.com>
2016-12-29 11:49:37 +0800

    Classroom Overview

--- src/components/FreeClassRoom.vue
+++ src/components/FreeClassRoom.vue
@@ -1,51 +1,29 @@
 <template>
-<div>
-  <h1 class="page-title">Salles Disponibles </h1>
-  <hr/>
-
-  
-  <div class="container-btn">
-    <a class="button">O+301</a>
-    <a class="button">E+135</a>
-    <a class="button">E+141</a>
+  <div>
+    <h1 class="page-title">Salle de cours</h1>
+    <hr />
+    <div class="page-content">
+      <classroom-overview classroom_name="E+130" :nb_students_inside="4" :charger="true" :projector="true"></classroom-overview>
+      <classroom-overview classroom_name="E+131" :nb_students_inside="0" :charger="true" :projector="false"></classroom-overview>
+      <classroom-overview classroom_name="O+302" :nb_students_inside="9" :charger="true" :projector="false"></classroom-overview>
+    </div>
+    <backbutton></backbutton>
   </div>
 
-  <backbutton></backbutton>
-</div>
 </template>
 
 <script>
-import Backbutton from './common/BackButton'
+  import Backbutton from './common/BackButton'
+  import ClassroomOverview from './common/ClassroomOverview'
 
-export default {
-  name: 'FreeClassRoom',
-  components: {
-    Backbutton
+  export default {
+    name: 'FreeClassRoom',
+    components: {
+      Backbutton,
+      ClassroomOverview
+    }
   }
-}
 </script>
 
-<style scoped>
-	time {
-		font-family: serif;
-		font-size: 25px;
-	}
-
-	.center {
-		text-align: center;
-	}
-
-	.title {
-		font-family: serif;
-	}
-
-	.window {
-		text-align: center;
-		margin-left: auto;
-		margin-right: auto;
-		margin-top: 20px;
-		margin-bottom: 20px;
-		width: 80%;
-
-	}
+<style lang="scss" scoped>
 </style>
---
+++ src/components/common/ClassroomOverview.vue
@@ -0,0 +1,63 @@
+<template>
+  <router-link :id="classroom" class="button" :to="'Room/'+classroom">
+    <span id="classroom_name" class="classroom-info tab">{{ classroom_name }}</span>
+    <img id="classroom_people_icon" class="classroom-icon tab" src="./../../images/people.png"/>
+    <span id="classroom_people_number" class="classroom-info">{{ nb_students_inside }}</span>
+    <img id="classroom_charger_icon" class="classroom-icon tab" v-show="show_charger" src="./../../images/charger.png" />
+    <img id="classroom_projector_icon" class="classroom-icon tab" v-show="show_projector" src="./../../images/projector.png" />
+
+  </router-link>
+</template>
+
+<script>
+
+  export default {
+    name: 'ClassroomOverview',
+    props: ['classroom_name','nb_students_inside','charger','projector'],
+    mounted: function(){
+      this.initCSS();
+
+    },
+    methods: {
+      initCSS: function() {
+        if (this.nb_students_inside == 0) {
+          document.getElementById(this.classroom_name).setAttribute('class', 'button free');
+        }
+        else {
+          document.getElementById(this.classroom_name).setAttribute('class', 'button occupied');
+        }
+
+      }
+    },
+    data: function() {
+      return { classroom: this.classroom_name, show_charger: this.charger , show_projector: this.projector }
+    }
+
+  }
+
+</script>
+
+<style lang="scss" scoped>
+
+  .button {
+    text-align: left;
+  }
+  .tab {
+    margin-left: 10%;
+  }
+  .free {
+    background-color: green;
+  }
+  .occupied {
+    background-color: orange;
+  }
+  .classroom-icon {
+    display: inline-block;
+    width: 10%;
+  }
+  .classroom-info {
+    vertical-align: 50%;
+    color: black;
+  }
+
+</style>
