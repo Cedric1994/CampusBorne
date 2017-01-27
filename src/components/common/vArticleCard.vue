ef07fcb0a8ca30357022938fffd897f8a957711d
Thomas Clop <clop@polytech.unice.fr>
2017-01-27 14:53:38 +0100

    fix play pause

--- src/components/common/vArticleCard.vue
+++ src/components/common/vArticleCard.vue
@@ -1,9 +1,10 @@
 <template lang="jade">
-  article(@click="toggleFullScreen", :class="{ 'active': isActive }")
+  article(:class="{ 'active': isActive }")
     img(:src="illustration", alt="Article illustration")
     .text
       h1="{{title}}"
       .description(v-for="description in descriptionList")="{{description}}"
+    button(@click="toggleFullScreen")="Read more... {{isActive}}"
 </template>
 
 <script>
@@ -72,12 +73,19 @@
     text-align: justify;
     user-select: none;
     white-space: normal;
-    overflow: auto;
+    overflow-y: hidden;
     height: calc(100% - 250px);
   }
 
   .description {
     margin-bottom: 10px;
+  }
+
+  button{
+    margin: 0 5px 5px 0;
+    float: right;
+    background: transparent;
+    border: none;
   }
 
 </style>
